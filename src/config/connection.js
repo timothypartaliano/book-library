b9a26b6c290564e581289dae2951ac6592696471
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-03 15:25:22 +0700

    refactor conn

--- src/config/connection.js
+++ src/config/connection.js
@@ -20,23 +20,3 @@ sequelize
   .catch((error) => console.error('Error connecting to the database:', error));
 
 module.exports = sequelize;
-
-/*Connect using postgres option*/
-// const { Client } = require('pg')
-// const dotenv = require('dotenv')
-
-// dotenv.config();
- 
-// const client = new Client({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-// })
-
-// client.connect()
-//   .then(() => console.log('Connected to the database'))
-//   .catch(error => console.error('Error connecting to the database', error))
-
-// module.exports = client
